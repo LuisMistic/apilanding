3f45a16d21fc74fed1a2f57fc4c43089f47ef325
Luis Martinez <luis.escalada@hotmail.com>
2023-05-07 18:47:12 -0300

    estructura base api

---
+++ src/test/java/com/api/landing/LandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.landing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
